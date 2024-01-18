dc6181f9385f6256ab093fce0a730409befa2e0b
Julien Martel <julien@monogram.io>
2024-01-17 19:01:44 -0600

    sync

--- idx-template.nix
+++ idx-template.nix
@@ -8,10 +8,10 @@
 
     # Available options as of 1/17/2024
     # https://github.com/nuxt/cli/blob/f113a083f000d19c9ae7f35ae2534ac5c0dba77b/src/commands/init.ts
-    # npx nuxi@latest init nuxt-idx --package-manager pnpm --install true --git-init true --force true
+    # npx nuxi@latest init nuxt-idx --package-manager bun --install true --git-init true --force true
 
     # To test this configuration:
-    # /nix/store/mvr5wczap3ga80iq548n2griy8kx9ksx-idx-template/bin/idx-template ~/Monospace/workspace/nix_templates/nuxt --output-dir ~ --workspace-name foo -a '{"packageManager": "yarn", "install": true}'
+    # /nix/store/mvr5wczap3ga80iq548n2griy8kx9ksx-idx-template/bin/idx-template ~/Monospace/workspace/nix_templates/nuxt --output-dir ~ --workspace-name foo -a '{"packageManager": "bun"}'
 
     bootstrap = ''
       bunx nuxi@latest -y init "$out" \
